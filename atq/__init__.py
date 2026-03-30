f6566266257bdc2eaa1eccf451f91d3fbc640308
Aditya <adityaswaoop23@gmail.com>
2026-03-30 09:48:57 -0500

    chore: clean up atq/__init__.py now that all modules exist

--- atq/__init__.py
+++ atq/__init__.py
@@ -1,25 +1,9 @@
 """ATQ - Adaptive Ternary Quantization for LLM Compression."""
 
 from atq.quantizers import ternary_quantize, adaptive_threshold_magnitude, adaptive_threshold_sparsity
-
-try:
-    from atq.layers import TernaryLinear
-except ImportError:
-    pass
-
-try:
-    from atq.bit_packing import pack_ternary, unpack_ternary
-except ImportError:
-    pass
-
-try:
-    from atq.mixed_precision import compute_layer_importance, assign_precision
-except ImportError:
-    pass
-
-try:
-    from atq.calibration import calibrate_thresholds
-except ImportError:
-    pass
+from atq.layers import TernaryLinear
+from atq.bit_packing import pack_ternary, unpack_ternary
+from atq.mixed_precision import compute_layer_importance, assign_precision
+from atq.calibration import calibrate_thresholds
 
 __version__ = "0.1.0"
